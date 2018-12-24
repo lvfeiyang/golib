fa4c3c4391413549e7cadbaa494483e0211e871f
吕晓骏 <lvxiaojun@qutoutiao.net>
2018-12-25 01:12:20 +0800

    add redis client

---
+++ redisclient/redisclient.go
@@ -0,0 +1,29 @@
+package redisclient
+
+import (
+	"github.com/gomodule/redigo/redis"
+)
+
+type RedisConfig struct {
+	name string
+	ip   string
+	db   int
+}
+
+var G_RedisClient = make(map[string]redis.Conn)
+
+func Redisinit(rcs []RedisConfig) error {
+	for _, rc := range rcs {
+		if conn, err := redis.Dial("tcp", rc.ip, redis.DialDatabase(rc.db)); err != nil {
+			return err
+		} else {
+			G_RedisClient[rc.name] = conn
+		}
+	}
+	return nil
+}
+
+func RedisGetClient(name string) (conn redis.Conn, exist bool) {
+	conn, exist = G_RedisClient[name]
+	return
+}
